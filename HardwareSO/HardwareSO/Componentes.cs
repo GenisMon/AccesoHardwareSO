2f468dfeb66ce1cbd841d496b96a058fbcdd69a9
GenisMon <genis.monsalve164@pascualbravo.edu.co>
2022-10-31 15:57:30 -0500

    Agregar archivos de proyecto.

---
+++ HardwareSO/HardwareSO/Componentes.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Management.Instrumentation;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Net;
+using WUApiLib;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace HardwareSO
+{
+    public class Componentes
+    {
+        public static string Serial(string NroSerial)
+        {
+            ManagementObjectSearcher buscador = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PhysicalMedia");
+            foreach (ManagementObject objeto in buscador.Get())
+            {
+                if (objeto["SerialNumber"] != null)
+                {
+                    NroSerial = objeto["SerialNumber"].ToString();
+                    return NroSerial;
+                }
+            }
+            return null;
+        }
+
+        public static string UnidadesDeDisco()
+        {
+            DriveInfo[] Unidades = DriveInfo.GetDrives();
+            string Disco = "";
+            foreach (DriveInfo Unidad in Unidades)
+            {
+                Disco+= ($"Unidad {Unidad.Name}\n " + $"Tipo: {Unidad.DriveType}\n " + $"Espacio libre: {(Unidad.AvailableFreeSpace) / 1073741824 + " GB"}\n");
+
+            }
+            return Disco;
+        }
+
+        public static string NumeroDeProcesadores()
+        {
+            return ($"Numero de procesadores: {Environment.ProcessorCount}");
+        }
+
+        public static string Ram()
+        {
+            ManagementObjectSearcher Busqueda = new ManagementObjectSearcher("Select * From Win32_ComputerSystem");
+            foreach (ManagementObject Objeto in Busqueda.Get())
+            {
+                double Ram_Bytes = (Convert.ToDouble(Objeto["TotalPhysicalMemory"]));
+                return ($"RAM Utilizable: {(Ram_Bytes) / 1073741824} GB");
+            }
+            return null;
+        }
+
+        public static string NIC()
+        {
+            NetworkInterface[] interfaz = NetworkInterface.GetAllNetworkInterfaces();
+            string red = "";
+            foreach (NetworkInterface adaptador in interfaz)
+            { 
+                red += ($"Name: {adaptador.Name}\n " + $"Descripcion: {adaptador.Description}\n " + $"Tipo de interfaz: {adaptador.NetworkInterfaceType}\n " + $"Estado operacional: {adaptador.OperationalStatus}\n " + "--------\n");
+            }
+
+            return red;
+        }
+
+        public static string MacAddress()
+        {
+            foreach (NetworkInterface NIC in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                return NIC.GetPhysicalAddress().ToString();
+            }
+            return null;
+        }
+
+        public static string Patches()
+        {
+            var sesionActualización = new UpdateSession();
+            var actualizarBuscador = sesionActualización.CreateUpdateSearcher();
+            var cont = actualizarBuscador.GetTotalHistoryCount();
+            var historia= actualizarBuscador.QueryHistory(0, cont);
+            string mostrar = "";
+
+            for (int i = 0; i < cont; ++i)
+            {
+                mostrar += (historia[i].Title + "\n");
+            }
+            return mostrar;
+        }
+
+        public static string LeerClave(string num1, string num2)
+        {
+            return Registry.GetValue(num1, num2, "Registro no encontrado.").ToString();
+        }
+        public static void EstablecerClave(string num1, string num2, string num3)
+        {
+            Registry.SetValue(num1, num2, num3);
+        }
+        public static string CrearClave(string num1, string num2, string num3)
+        {
+            Registry.SetValue(num1, num2, num3);
+            return Registry.GetValue(num1, num2, "Registro no encontrado.").ToString();
+        }
+        public static void EliminarClave(string num1, string num2)
+        {
+            if (num2 != null)
+            {
+                File.WriteAllText("sys.reg", "Windows Registry Editor Version 5.00" + "\n" + "\n" + "[" + num1 + "]" + "\n" + "\"" + num2 + "\"=-");
+                Process.Start(@"sys.reg");
+            }
+            else
+            {
+                 File.WriteAllText("sys.reg", "Windows Registry Editor Version 5.00" + "\n" + "\n" + "[-" + num1 + "]");
+                Process.Start(@"sys.reg");
+            }
+        }
+
+    }
+}
---
+++ IntegradoComponentes/IntegradoComponentes/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using HardwareSO;
+
+namespace IntegradoComponentes
+{
+    internal class Integrado
+    {
+        public void Inicial()
+        {
+            string serialnro = "";
+            string nroSerie = Componentes.Serial(serialnro);
+            Console.WriteLine("Serial del disco duro: " + nroSerie);
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            string nroDisco = Componentes.UnidadesDeDisco();
+            Console.WriteLine(nroDisco);
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            string nroProcesadores = Componentes.NumeroDeProcesadores();
+            Console.WriteLine(nroProcesadores);
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            string ram = Componentes.Ram();
+            Console.WriteLine(ram);
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            string nic = Componentes.NIC();
+            Console.WriteLine(nic);
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            string macAnddress = Componentes.MacAddress();
+            Console.WriteLine("MacAnddress: " + macAnddress);
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+            string patches = Componentes.Patches();
+            Console.WriteLine("Patches: " + patches);
+            Console.WriteLine("-------------------------------------------------------------------------------------------------");
+        }
+
+        public void Registro()
+        {
+            bool ban = false;
+            string path, nomClave, valorClave;
+            while (!ban)
+            {
+                Console.WriteLine("Elige una opcion");
+                Console.WriteLine("1. Crear clave");
+                Console.WriteLine("2. Leer clave");
+                Console.WriteLine("3. Modificar");
+                Console.WriteLine("4. Eliminar");
+                Console.WriteLine("5. Salir");
+                int opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Crear clave");
+                        path = Console.ReadLine();
+                        nomClave = Console.ReadLine();
+                        valorClave = Console.ReadLine();
+                        Componentes.CrearClave(path, nomClave, valorClave);
+                        Console.WriteLine("Clave creada correctamente");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Leer clave");
+                        path = Console.ReadLine();
+                        nomClave = Console.ReadLine();
+                        valorClave = Componentes.LeerClave(path, nomClave);
+                        Console.WriteLine(valorClave);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Modificar clave");
+                        path = Console.ReadLine();
+                        nomClave = Console.ReadLine();
+                        valorClave = Console.ReadLine();
+                        Componentes.EstablecerClave(path, nomClave, valorClave);
+                        Console.WriteLine("Clave modificada correctamente");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Eliminar clave");
+                        path = Console.ReadLine();
+                        nomClave = Console.ReadLine();
+                        Componentes.EliminarClave(path, nomClave);
+                        Console.WriteLine("Clave eliminada correctamente");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Saliste del registro del sistema");
+                        ban = true;
+                        break;
+                }
+            }
+        }
+
+        public  void ObtenerProceso()
+        {
+            Process[] proc;
+            proc = Process.GetProcesses();
+            foreach (Process p in proc)
+            {
+                string pr = p.ProcessName;
+                Console.WriteLine(pr);
+            }
+
+        }
+
+        public void Proceso(string proceso)
+        {
+            
+            Process[] process = Process.GetProcessesByName(proceso);
+            foreach (var proce in process)
+            {
+                proce.Kill();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            bool ban = false;
+            string sn;
+            Integrado integrado = new Integrado();
+             while (!ban)
+             {
+                 Console.WriteLine("Elige que metodos deseas: ");
+                 Console.WriteLine("1. Principal: \r\n - Leer el nùmero seri del Disco Duro / CD / DVD\r\n - ¿Cúantas unidades de disco tiene?\r\n - Balance general del sistema : Procesadores, RAM, NIC, patches.\r\n - Obtener MAC Address.");
+                 Console.WriteLine("2. Acceso al Registro del Sistema");
+                 Console.WriteLine("3. Obtener los procesos activos / *matar*  procesos.");
+                 Console.WriteLine("4. Salir");
+                 int opcion = int.Parse(Console.ReadLine());
+                 switch (opcion)
+                 {
+                     case 1:
+                         integrado.Inicial();
+                         Console.WriteLine("Deseas limpiar la consola Si/No");
+                         sn = Console.ReadLine();
+                         if (sn == "SI" || sn == "Si" || sn == "si")
+                         {
+                             Console.Clear();
+                         }
+
+                         break;
+
+                     case 2:
+                         integrado.Registro();
+                         Console.WriteLine("Deseas limpiar la consola Si/No");
+                         sn = Console.ReadLine();
+                         if (sn == "SI" || sn == "Si" || sn == "si")
+                         {
+                             Console.Clear();
+                         }
+
+                         break;
+
+                     case 3:
+                        Console.WriteLine("---------------Acceso a procesos---------------");
+                        integrado.ObtenerProceso();
+                        Console.WriteLine("----------------------------------------------------------");
+                         Console.WriteLine("Ingrese el nombre del proceso a detener: ");
+                         string procesos = Console.ReadLine();
+                         integrado.Proceso(procesos);
+                         Console.WriteLine("Deseas limpiar la consola Si/No");
+                         sn = Console.ReadLine();
+                         if (sn == "SI" || sn == "Si" || sn == "si")
+                         {
+                             Console.Clear();
+                         }
+
+                         break;
+
+                     case 4:
+                         Console.WriteLine("Saliste del programa");
+                         ban = true;
+                         break;
+                 }
+             }
+            Console.ReadKey();
+        }
+    }
+}
